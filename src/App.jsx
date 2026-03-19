2090b0c85cdb718862efa2f3d9f2020dcbe2802c
ltherisod <lauratherisod@gmail.com>
2026-03-19 20:58:15 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ItemListContainer from "./components/ItemListContainer"
+function App() {
+ 
+
+  return (
+    <div>
+     <h1>Bienvenidos a React!</h1>
+     <ItemListContainer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+const ItemListContainer = ()=> {
+    return(
+        <div>
+            <h1>Hola soy un componente</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
+
